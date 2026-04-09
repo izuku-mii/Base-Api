12084fd63630bd5448dc410fa32903dd89a8e250
Izuku-mi <151708571+izuku-mii@users.noreply.github.com>
2026-04-09 21:07:40 +0700

    Create imagev2.js

---
+++ api/post/image/imagev2.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage()
+});
+
+module.exports = {
+    category: 'post',
+    path: true,
+    post: true,
+    paramsFile: ["image"],
+    paramsPost: ["message", "message2"]
+    async run(req, res) {
+        const { message, message2 } = req.body;
+        upload.single('image')(req, res, async (err) => {
+            try {
+                if (err) {
+                    return res.status(400).json({ error: err.message });
+                }
+                if (!req.file) {
+                    return res.status(400).json({ error: 'Image is required' });
+                }
+                const buffer = req.file.buffer;
+                return { buffer, message, message2 };
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        });
+    }
+};
